959951a6e3c6af257eb15db206449fd99a564c26
Pirate Cool <33762776+BrokenPirate@users.noreply.github.com>
2018-10-15 18:07:39 +0200

    Update Game.rb

--- Classes/Game.rb
+++ Classes/Game.rb
@@ -1,6 +1,25 @@
 class Game
   def initialize
     #TO DO : créé 2 joueurs, créé un board
+    
+      def initialize
+    
+            puts "Player 1 will be playing X -> Whats Player 1 name ?"
+            # Creer joueur 1
+            @player1 = Player.new(@player_name, "x")
+            puts "Player 2 will be playing O -> Whats Player 2 name ?"
+            # Creer joueur 2
+            @player2 = Player.new(@player_name, "o")
+            puts "#{@player1.player_name} will play X"
+            puts "#{@player2.player_name} will play O"
+            
+            @board_game = Board.new
+            @current_player = @player1
+            @player1.player_team = "X"
+            @player2.player_team = "O"
+     end
+    
+    
   end
 
   def go
