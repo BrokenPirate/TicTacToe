ab55783bd9ce3807657950960b8ee89a268b0220
OwwllMan <39001414+OwwllMan@users.noreply.github.com>
2018-10-15 18:06:37 +0200

    prout

--- Classes/Player.rb
+++ Classes/Player.rb
@@ -4,9 +4,8 @@ class Player
 attr_accessor :player_name, :player_team
     attr_writer  :player_points
  
-  
+   
     #TO DO : doit régler son nom, sa valeur, son état de victoire
-
     def initialize(player_name, player_team)
 
     	@player_name = gets.chomp
