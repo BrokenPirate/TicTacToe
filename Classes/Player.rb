d4ec5621841a449555e79a6d941d9384b8d470fb
Pirate Cool <33762776+BrokenPirate@users.noreply.github.com>
2018-10-15 18:05:38 +0200

    Update Player.rb

--- Classes/Player.rb
+++ Classes/Player.rb
@@ -1,29 +1,17 @@
 class Player
-  
-  attr_accessor :player_name_1, :player_name_2
 
-  def init
+
+attr_accessor :player_name, :player_team
+    attr_writer  :player_points
+ 
+  
     #TO DO : doit régler son nom, sa valeur, son état de victoire
 
-    puts "Joueur 1 choisis ton nom"
-    print ">"
-    @player_name_1 = gets.chomp
+    def initialize(player_name, player_team)
 
-    puts "Joueur 2 choisis ton nom"
-    print ">"
-    @player_name_2 = gets.chomp
+    	@player_name = gets.chomp
+    	@player_team = ""
 
-    puts "Bienvenue sur TicTacToe #{@player_name_1}, tu as les croix et #{player_name_2}, tu as les ronds. Bon jeu :)"
-  end
-
-  def welcome
-
-    "Bienvenue sur TicTacToe #{@player_name_1} et #{player_name_2}"
-
-  end
+    end
   
 end
-
-newgame = Player.new
-newgame.init
-newgame.welcome
