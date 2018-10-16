9d8b490260d422780a2151df7c1b9ad05916e5fd
BrokenPirate <i.am.ugogessa@gmail.com>
2018-10-16 10:03:40 +0200

    update

--- Classes/Game.rb
+++ Classes/Game.rb
@@ -3,8 +3,18 @@ class Game
     #TO DO : créé 2 joueurs, créé un board
     
       def initialize
-    
-
+      	    puts "Player 1 will be playing X -> Whats Player 1 name ?"
+            @player1 = Player.new(@player_name, "x")
+            puts "Player 2 will be playing O -> Whats Player 2 name ?"
+            @player2 = Player.new(@player_name, "o")
+            puts "#{@player1.player_name} will play X"
+            puts "#{@player2.player_name} will play O"
+            
+            @board_game = Board.new
+            @current_player = @player1
+            @player1.player_team = "X"
+            @player2.player_team = "O"
+            @victory == false
      end
     
     
@@ -12,10 +22,26 @@ class Game
 
   def go
     # TO DO : lance la partie
+    puts "Let's go !"
+    @board_game.show
+    puts "It's #{@current_player.player_name} turn!"
+    turn
+
   end
 
   def turn
     #TO DO : affiche le plateau, demande au joueur il joue quoi, vérifie si un joueur a gagné, passe au joueur suivant si la partie n'est pas finie
+ 	
+ 	while victory? != true
+ 	@choice = gets.chomp.to_i
+ 	if @choice.between?(1..9)
+ 		play(@choice, @current_player, @current_player.player_team)
+ 	else
+ 		puts "Choose an available case between 1 and 9"
+ 	end
+ 	@current_player == @player1 ? @current_player = @player2 : @current_player = @player1
+	end
+
   end
 
 end
